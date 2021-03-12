0cc9805537e1fb9b0bd58378adc6e9672d6ee521
Jared Hanson <jred0011@gmail.com>
2021-03-11 23:42:00 -0800

    Fix exception handling

--- modules/hawk/server/__init__.py
+++ modules/hawk/server/__init__.py
@@ -12,7 +12,7 @@ api = summ_web.create_api(app)
 summ_web.setup_webargs()
 jwt = summ_web.create_jwt(app)
 jwt.load_user = load_user_fn(session=db.session)
-summ_web.setup_exception_handling(app, err_code_to_status_code)
+summ_web.setup_exception_handling(api, err_code_to_status_code)
 
 from . import routes
 
