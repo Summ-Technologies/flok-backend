a4961c7278716fbb897f46c78c19ee0e602ad730
Jared Hanson <jred0011@gmail.com>
2021-07-27 02:22:48 -0700

    Added table + endpoint

--- lib/hawk-core/src/hawk_core/hawk_managers.py
+++ lib/hawk-core/src/hawk_core/hawk_managers.py
@@ -1,4 +1,9 @@
 import logging
+from typing import Optional
+
+from hawk_db.lodging import LodgingProposalRequest
+from marshmallow.fields import List
+from sqlalchemy.sql.sqltypes import Date
 
 from .base_manager import BaseManager
 
@@ -7,3 +12,31 @@ logger = logging.getLogger(__name__)
 
 class UserManager(BaseManager):
     pass
+
+
+class LodgingManager(BaseManager):
+    def create_lodging_proposal_request(
+        self,
+        number_attendees: int,
+        flexible_dates: bool,
+        occupancy_types: Optional[List],
+        meeting_spaces: Optional[List],
+        number_nights: Optional[int],
+        preferred_months: Optional[List],
+        preferred_start_dow: Optional[List],
+        start_date: Optional[Date],
+        end_date: Optional[Date],
+    ):
+        new_request = LodgingProposalRequest()
+        new_request.number_attendees = number_attendees
+        new_request.flexible_dates = flexible_dates
+        new_request.occupancy_types = occupancy_types
+        new_request.meeting_spaces = meeting_spaces
+        new_request.number_nights = number_nights
+        new_request.preferred_months = preferred_months
+        new_request.preferred_start_dow = preferred_start_dow
+        new_request.start_date = start_date
+        new_request.end_date = end_date
+        self.session.add(new_request)
+        self.session.flush()
+        return new_request
